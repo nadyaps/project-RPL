55523916fe7bd87c5f9afa228c03a62691a6acaf
NameCard <sadinp21d@student.unhas.ac.id>
2024-04-01 22:26:11 +0800

    breeze dan create database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
+            $table->integer('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('photo')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['admin','user','guest'])->default('guest');
             $table->rememberToken();
             $table->timestamps();
         });
